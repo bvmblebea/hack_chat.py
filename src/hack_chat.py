cd210d645adddda1286671001de45c061fe3fdae
ecclesias <77536370+zeviel@users.noreply.github.com>
2022-11-19 18:31:24 +0800

    Update hack_chat.py

--- src/hack_chat.py
+++ src/hack_chat.py
@@ -1,117 +1,136 @@
 import websocket
+from json import dumps
+from json import loads
 from time import sleep
 from threading import Thread
-from json import dumps, loads
-
 
 class HackChat:
-    def __init__(
-            self,
-            nickname: str,
-            password: str = None,
-            channel: str = "programming"):
-        self.nickname = nickname
-        self.password = password
-        self.channel = channel
-        self.on_join = []
-        self.on_leave = []
-        self.messages = []
-        self.statistics = []
-        self.online_users = []
-        self.ws = websocket.create_connection("wss://hack.chat/chat-ws")
-        self.send_packet({"cmd": "join",
-                          "channel": self.channel,
-                          "nick": self.format_nickname(self.nickname,
-                                                       self.password)})
-        Thread(target=self.ping_thread).start()
+	def __init__(
+			self,
+			nickname: str,
+			password: str = None,
+			channel: str = "programming") -> None:
+		self.nickname = nickname
+		self.password = password
+		self.channel = channel
+		self.on_join = []
+		self.on_leave = []
+		self.messages = []
+		self.statistics = []
+		self.online_users = []
+		self.ws = websocket.create_connection("wss://hack.chat/chat-ws")
+		self.send_packet(
+			{
+				"cmd": "join",
+				"channel": self.channel,
+				"nick": self.format_nickname(self.nickname, self.password)
+			}
+		)
+		Thread(target=self.ping_thread).start()
 
-    # send packet
-    def send_packet(self, packet: dict):
-        self.ws.send(dumps(packet))
+	def send_packet(self, packet: dict) -> None:
+		self.ws.send(dumps(packet))
 
-    def format_nickname(self, nickname: str, password: str = None):
-        if password:
-            self.nickname = f"{nickname}#{password}"
-        return nickname
+	def format_nickname(
+			self,
+			nickname: str,
+			password: str = None) -> str:
+		if password:
+			self.nickname = f"{nickname}#{password}"
+		return nickname
 
-    # send message in chat
-    def send_message(self, message: str):
-        self.send_packet({"cmd": "chat", "text": message})
+	def send_message(self, message: str) -> None:
+		self.send_packet({"cmd": "chat", "text": message})
 
-    # send message to user
-    def send_message_to(self, nickname: str, message: str):
-        self.send_packet({"cmd": "whisper", "nick": nickname, "text": message})
+	def send_message_to(
+			self,
+			nickname: str,
+			message: str) -> None:
+		self.send_packet(
+			{"cmd": "whisper", "nick": nickname, "text": message}
+		)
 
-    # change current nickname to new
-    def change_nickname(self, nickname: str):
-        self.nickname = nickname
-        self.send_packet({"cmd": "changenick", "nick": nickname})
+	def change_nickname(self, nickname: str) -> None:
+		self.nickname = nickname
+		self.send_packet({"cmd": "changenick", "nick": nickname})
 
-    # moves from current channel to other channel
-    def move_from_channel(self, channel: str):
-        self.channel = channel
-        self.send_packet({"cmd": "move", "channel": channel})
+	def move_from_channel(self, channel: str) -> None:
+		self.channel = channel
+		self.send_packet({"cmd": "move", "channel": channel})
 
-    # invites user to a randomly generated channel
-    def invite_user(seld, nickname: str):
-        self.send_packet({"cmd": "invite", "nick": nickname})
+	def invite_user(seld, nickname: str) -> None:
+		self.send_packet({"cmd": "invite", "nick": nickname})
 
-    # get statistics
-    def get_statistics(self):
-        self.send_packet({"cmd": "stats"})
+	def get_statistics(self) -> None:
+		self.send_packet({"cmd": "stats"})
 
-    # Ban <nickname> from hack.chat for 24 hours you need moderator or admin
-    # <password>
-    def ban_user(self, nickname: str):
-        self.send_packet({"cmd": "ban", "nick": nickname})
+	def ban_user(self, nickname: str) -> None:
+		"""
+		REQUIRES:
+			password - of admin or moderator
+		"""
+		self.send_packet({"cmd": "ban", "nick": nickname})
 
-    # Unban <nickname> you need moder or admin <password>
-    def unban_user(self, user_ip: str):
-        self.send_packet({"cmd": "unban", "ip": user_ip})
+	def unban_user(self, user_ip: str) -> None:
+		"""
+		REQUIRES:
+			password - of admin or moderator
+		"""
+		self.send_packet({"cmd": "unban", "ip": user_ip})
 
-    # kick user to another channel for this you need moderator <password>
-    def kick_user(self, nickname: str):
-        self.send_packet({"cmd": "kick", "nick": nickname})
 
-    # Makes the user a moderator for this you need admin <password>
-    def add_moderator(self, nickname: str):
-        self.send_packet({"cmd": "addmod", "nick": nickname})
+	def kick_user(self, nickname: str) -> None:
+		"""
+		REQUIRES:
+			password - of admin or moderator
+		"""
+		self.send_packet({"cmd": "kick", "nick": nickname})
 
-    # Saves hack.chat current config. Only for admins
-    def save_config(self):
-        self.send_packet({"cmd": "saveconfig"})
+	def add_moderator(self, nickname: str) -> None:
+		"""
+		REQUIRES:
+			password - of admin or moderator
+		"""
+		self.send_packet({"cmd": "addmod", "nick": nickname})
 
-    # listen
-    def listen(self):
-        Thread(target=self.on_message).start()
+	def save_config(self) -> None:
+		self.send_packet({"cmd": "saveconfig"})
 
-    # on_message
-    def on_message(self):
-        while True:
-            response = loads(self.ws.recv())
-            if response["cmd"] == "chat":
-                for handler in list(self.messages):
-                    handler(self, response["text"], response["nick"])
-            elif response["cmd"] == "onlineAdd":
-                self.online_users.append(response["nick"])
-                for handler in list(self.on_join):
-                    handler(self, response["nick"])
-            elif response["cmd"] == "onlineRemove":
-                self.online_users.remove(response["nick"])
-                for handler in list(self.on_leave):
-                    handler(self, response["nick"])
-            elif response["cmd"] == "onlineSet":
-                for nickname in response["nicks"]:
-                    self.online_users.append(nickname)
-            elif response["cmd"] == "info" and response.get("type") == "whisper":
-                for handler in list(self.on_whisper):
-                    handler(self, response["text"], response["from"], response)
-            elif response["cmd"] == "info" and " IPs " in response["text"]:
-                for handler in list(self.statistics):
-                    handler(self, response["text"])
+	def listen(self) -> None:
+		Thread(target=self.on_message).start()
 
-    # ping to retain websocket connection
-    def ping_thread(self):
-        while self.ws.connected:
-            self.send_packet({"cmd": "ping"})
-            sleep(60)
+	def on_message(self) -> None:
+		while True:
+			response = loads(self.ws.recv())
+			if response["cmd"] == "chat":
+				for handler in list(self.messages):
+					handler(
+						self,
+						response["text"],
+						response["nick"])
+			if response["cmd"] == "onlineAdd":
+				self.online_users.append(response["nick"])
+				for handler in list(self.on_join):
+					handler(self, response["nick"])
+			if response["cmd"] == "onlineRemove":
+				self.online_users.remove(response["nick"])
+				for handler in list(self.on_leave):
+					handler(self, response["nick"])
+			if response["cmd"] == "onlineSet":
+				for nickname in response["nicks"]:
+					self.online_users.append(nickname)
+			if response["cmd"] == "info" and response.get("type") == "whisper":
+				for handler in list(self.on_whisper):
+					handler(
+						self,
+						response["text"],
+						response["from"],
+						response)
+			if response["cmd"] == "info" and " IPs " in response["text"]:
+				for handler in list(self.statistics):
+					handler(self, response["text"])
+
+	def ping_thread(self) -> None:
+		while self.ws.connected:
+			self.send_packet({"cmd": "ping"})
+			sleep(60)
